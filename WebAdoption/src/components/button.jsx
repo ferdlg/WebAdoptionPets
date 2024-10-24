53488b508c3f98229fe222fd611d36e712828c68
Dayana Garzon <haybet_garzon@soy.sena.edu.co>
2024-10-23 21:11:19 -0500

    agregacion de componentes como button y card

---
+++ WebAdoption/src/components/button.jsx
@@ -0,0 +1,13 @@
+
+import './Button.css';  
+
+const Button = ({ label, onClick }) => {
+  return (
+    <button onClick={onClick} className="button">
+      {label}
+    </button>
+  );
+};
+
+export default Button;
+
