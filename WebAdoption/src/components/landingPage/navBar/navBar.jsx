d97510f07cb7eb238a17416b0a1bad38dce09ffc
ferdlg <ferdlgpar@gmail.com>
2024-10-29 16:08:15 -0500

    Estilos: Nav Bar

--- WebAdoption/src/components/landingPage/navBar/navBar.jsx
+++ WebAdoption/src/components/landingPage/navBar/navBar.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../../assets/styles/components/navBar.css';
+import { SiDatadog } from "react-icons/si";
+
 
 const NavBar = () => {
   return (
     <nav className="nav-bar">
       <ul className="nav-bar__list">
         <li className="nav-bar__item"><Link to="/" className="nav-bar__link">Home</Link></li>
-        <li className="nav-bar__item"><Link to="/how-to-adopt" className="nav-bar__link">¿Cómo Adoptar?</Link></li>
-        <li className="nav-bar__item"><Link to="/dogs" className="nav-bar__link">Perros</Link></li>
-        <li className="nav-bar__item"><Link to="/cats" className="nav-bar__link">Gatos</Link></li>
+        <li className="nav-bar__item"><Link to="/how-to-adopt" className="nav-bar__link">How to adopt?</Link></li>
+        <li className="nav-bar__item"><Link to="/dogs" className="nav-bar__link">Dogs</Link></li>
+        <li className="nav-bar__item"><Link to="/cats" className="nav-bar__link">Cats</Link></li>
+        <li className='nav-bar__item' ><Link className='nav-bar__link' to="/adoption-list"><SiDatadog size={40} className='.nav-bar__adoption-list-icon'/></Link></li>
       </ul>
     </nav>
   );
