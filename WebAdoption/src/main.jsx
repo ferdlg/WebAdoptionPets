80e25b4ea7d42298b10c7d5e4ae1efe99f280ff4
Eri1607 <eri16061142@gmail.com>
2024-10-28 19:09:14 -0500

    importe variables en el main.

--- WebAdoption/src/main.jsx
+++ WebAdoption/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './assets/styles/base/_index.css';
+import './assets/styles/utils/_variables.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
